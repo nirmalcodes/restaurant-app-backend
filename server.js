b7d11b8a4bd870f44600db0f90eeff293bfdb669
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-05-20 01:16:43 +0530

    Add winston to server.js

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const winston = require('winston');
 const orderRoutes = require("./routes/orders");
 
 // express app
 const app = express();
+
+// Configure winston logger
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'server.log' }),
+  ],
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+});
 
 // middleware
 app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
+  logger.info(`${req.method} ${req.url}`);
   next();
 });
 
@@ -24,7 +38,7 @@ mongoose
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log("Connected to DB Listening on Port", process.env.PORT);
+      console.log("connected to db & listening on port", process.env.PORT);
     });
   })
   .catch((error) => {
