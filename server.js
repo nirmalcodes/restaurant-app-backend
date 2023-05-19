0e405665ae962f04a4c81d9214ae5a19a133cd7a
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-05-20 01:17:43 +0530

    Update and comment out winston logs

--- server.js
+++ server.js
@@ -2,30 +2,30 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const winston = require('winston');
+// const winston = require('winston');
 const orderRoutes = require("./routes/orders");
 
 // express app
 const app = express();
 
 // Configure winston logger
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'server.log' }),
-  ],
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-});
+// const logger = winston.createLogger({
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({ filename: 'server.log' }),
+//   ],
+//   format: winston.format.combine(
+//     winston.format.timestamp(),
+//     winston.format.json()
+//   ),
+// });
 
 // middleware
 app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
-  logger.info(`${req.method} ${req.url}`);
+  // logger.info(`${req.method} ${req.url}`);
   next();
 });
 
